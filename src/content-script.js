01f00dade8489e320be011b03f3f95dfb16e2083
Matthew James <mjames@indeed.com>
2022-08-26 00:17:32 -0500

    Add counting of post reactions

--- src/content-script.js
+++ src/content-script.js
@@ -1,5 +1,6 @@
 const HIDDEN_ATTRIBUTE = 'hidden';
 const TOUCHED_ATTRIBUTE = 'on3-forum-highlighter';
+const MIN_REACTIONS = 10;
 
 chrome.storage.sync.get('enabled', ({ enabled }) => { run(enabled); });
 
@@ -14,7 +15,7 @@ function run(enabled) {
     if (posts) {
         Array.from(posts).forEach(post => {
             if (enabled) {
-                if (!post.getAttribute(HIDDEN_ATTRIBUTE)) {
+                if (shouldHide(post)) {
                     post.setAttribute(HIDDEN_ATTRIBUTE, 'true');
                     post.setAttribute(TOUCHED_ATTRIBUTE, 'true');
                 }
@@ -26,3 +27,22 @@ function run(enabled) {
     }
 }
 
+function shouldHide(post) {
+    if (post.getAttribute(HIDDEN_ATTRIBUTE)) {
+        return false;
+    }
+    return getReactionsCount(post) < MIN_REACTIONS;
+}
+
+function getReactionsCount(post) {
+    let count = 0;
+    const reactions = post.getElementsByClassName('reactionsBar-link')[0];
+    if (reactions) {
+        count += reactions.getElementsByTagName('bdi').length;
+        if (reactions.lastChild && reactions.lastChild.textContent.match(/ and \d+ others/)) {
+            count += parseInt(reactions.lastChild.textContent.trim().split(' ')[1]);
+        }
+    }
+
+    return count;
+}
